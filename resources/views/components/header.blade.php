ea40d5a41f3051b5ebb7bb66ac5382fb9c9ba43b
Inácio Barboza <108500066+InacioBrI@users.noreply.github.com>
2025-12-05 00:45:10 -0300

    Adiciona dependências de Heroicons e atualiza arquivos de configuração; implementa novo layout com cabeçalho e logo

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,28 @@
+<header class="w-full flex items-center justify-between px-20 py-4 border-b bg-white">
+
+    <div class="flex items-center space-x-3">
+         <img class="w-[50] h-[50px]" src="LogoFebasp.jpg" alt=""><!--logo belas artes  -->
+
+        <h1 class="text-xl font-medium text-black">
+            {{ $title ?? 'Achados e Perdidos' }}
+        </h1>
+    </div>
+
+    <div class="flex items-center space-x-6">
+
+        {{-- Ícone de Configurações (Login ou Config) --}}
+        @auth
+            <a href="{{ route('account.settings') }}">
+                <x-heroicon-o-cog class="w-7 h-7 text-black" />
+            </a>
+        @endauth
+
+        @guest
+            <a href="{{ route('login') }}" class="p-2 rounded-full hover:bg-gray-200 border">
+                <x-heroicon-s-user class="w-6 h-6 text-black "/>
+            </a>
+        @endguest
+
+    </div>
+</header>
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    @include('components.header', [
+        'title' => 'Achados e Perdidos',
+    ])
+
+    <div class="Flex flex-col items-center justify-center mt-10 px-70 m-auto space-y-6">
+        
+        <div class="flex flex-auto items-center justify-center bg-[#155dfc] h-25 text-amber-50 border rounded-xl p-4">
+            <h1>Perdeu algo na universidade? Estamos aqui para ajudar você a encontrar.</h1>
+        </div>
+
+        <a href="" class="flex flex-row items-center justify-center bg-transparent h-35 border border-b-black rounded-xl p-4 hover:border-blue-800 hover:bg-blue-800/10 transition-all duration-300 gap-2">
+            <div class=" px-5 py-5  m-3 bg-blue-800/50 rounded-full">
+                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 bg-blue-800 rounded-full text-amber-50 " >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                </svg>
+            </div>
+            <div class="flex flex-nowrap items-center justify-center"> 
+                <h1>Registre um item que você perdeu</h1>
+            </div>
+        </a>
+
+        <!-- <div class="flex items-center justify-center bg-[#fffbeb] h-25 text-black border-e-orange-600 rounded-xl p-4">
+            <h1>Entraremos em contato para mais informações sobre o objeto perdido.</h1>
+        </div> -->
+
+    </div>
+</body>
+</html>
